7aa975046de9de5452ce369a579386f2959f9b93
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-04-15 22:27:43 +0000

    question1

--- task_04.py
+++ task_04.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Some Docstring"""
-
 import car
 
+
 class CustomCar(car.Car):
+    def __init__(self, tires=None):
+        if tires == None:
+            self.tires = []
+            if len(self.tires) <= 4:
+                tire1 = CustomTire.tires
+                tire2 = CustomTire.tires
+                tire3 = CustomTire.tires
+                tire4 = CustomTire.tires
+                self.tires.append(tire1)
+                self.tires.append(tire2)
+                self.tires.append(tire3)
+                self.tires.append(tire4)
+        car.Car.__init__(self)
+
+
+class CustomTire(car.Tire):
+    tires = 1
+    _maximum_miles = 500
     
     def __init__(self):
-        car.Car.__init__(self)
+        car.Tire.__init__(self)
         
-        
-class CustomTire(car.Tire):
-    pass
-
 if __name__ == '__main__':
     mycar = CustomCar()
-    # len(mycar.tires)
+    print len(mycar.tires)
