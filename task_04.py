fe9b9860db198d1719bd1326bca91b898887068a
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-04-16 18:47:40 +0000

    question

--- task_04.py
+++ task_04.py
@@ -2,30 +2,31 @@
 # -*- coding: utf-8 -*-
 """Some Docstring"""
 import car
-
+import pprint
 
 class CustomCar(car.Car):
-    tires = []
-    def __init__(self, tires=None):
-        if tires == None:
+    
+    def __init__(self,color,tires=None):
+        self.tires = tires
+        if self.tires == None:
+            self.tires = []
+            tire = CustomTire()
             if len(self.tires) <= 4:
-                tire1 = CustomTire()
-                tire2 = CustomTire()
-                tire3 = CustomTire()
-                tire4 = CustomTire()
-                self.tires.append(tire1)
-                self.tires.append(tire2)
-                self.tires.append(tire3)
-                self.tires.append(tire4)
-        car.Car.__init__(self)
+                self.tires.append(tire)
+                self.tires.append(tire)
+                self.tires.append(tire)
+                self.tires.append(tire)
+        else:
+            pass
+        car.Car.__init__(self, color)
 
 
 class CustomTire(car.Tire):
+
     __maximum_miles = 500
     
-    #def __init__(self):
-    #    car.Tire.__init__(self)
-        
+
+
 if __name__ == '__main__':
-    mycar = CustomCar()
+    mycar = CustomCar('blue')
     print len(mycar.tires)
