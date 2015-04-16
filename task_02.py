35f55ce7153c85f8f8e9b4c8c7b44ec4af30bc04
gpsingh12 <gurpreet_1441@yahoo.com>
2015-04-16 04:03:47 +0000

    time

---
+++ task_02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task_02"""
+
+import time
+
+
+class Snapshot(object):
+    """ Docstring"""
+    def __init__(self):
+        self.created = time.time()
