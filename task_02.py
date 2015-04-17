5c9e0181f2ad6e4a491f7faec3858531942caa16
gpsingh12 <gurpreet_1441@yahoo.com>
2015-04-17 21:51:21 +0000

    snapshot

--- task_02.py
+++ task_02.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task_02"""
+"""Module provides functions related to time"""
 
 import time
 
 
 class Snapshot(object):
-    """ Docstring"""
+    """Object stores Unix Timestamp
+    Attribute:
+        None
+    """
+
     def __init__(self):
+        """ Constructor for Snapshot.
+        Arg:
+            None
+        Attribute:
+            created: output for time.time()
+        """
         self.created = time.time()
