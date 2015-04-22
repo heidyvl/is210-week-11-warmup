664b542200081cb880e5904e94eae6fd4e8787da
heiydvl <heidy.villarreal@spsmail.cuny.edu>
2015-04-22 23:30:42 +0000

    task_02

---
+++ task_02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Docstring"""
+
+import time
+
+
+class Snapshot(object):
+    """Class Snapshop"""
+    def __init__(self):
+        self.created = time.time()
